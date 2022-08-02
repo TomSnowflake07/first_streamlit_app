326999f5b7398ab367dc356443c2556e5fa5ccf4
TomSnowflake07 <109849406+TomSnowflake07@users.noreply.github.com>
2022-08-02 11:16:41 +0200

    Update streamlit_app.py
    fix bug prev commit. Refered to wrong var

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,4 +47,4 @@ streamlit.dataframe(my_data_rows)
 
 #allow end user to add a fruit to the list
 add_my_fruit= streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('Thanks for adding jackfruit', fruit_choice)
+streamlit.write('Thanks for adding jackfruit', add_my_fruit)
